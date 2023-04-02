f043db926673b6405c9b4c2cbef5b41c5c7d8dff
dcorbin15 <128358427+dcorbin15@users.noreply.github.com>
2023-04-02 05:13:42 -0700

    Add files via upload

---
+++ random/main.py
@@ -0,0 +1,23 @@
+# Darius Corbin
+# PA03
+# 2/21/2023
+#Create a Python program that converts a user input temperature in degrees Fahrenheit to degrees Celsius.
+#Create a Python program that converts a user input temperature in degrees Celsius to degrees Fahrenheit.
+import math
+
+def convert_to_cel(fahrenheit):
+    """Convert Fahrenheit to Celsius"""
+
+    return((fahrenheit - 32) * 5 / 9)
+
+def convert_to_F(celsius):
+    """Convert Celsius to Fahrenheit"""
+    return (celsius * (9/5) + 32  )
+
+fahrenheit = float(input('Enter a Fahrenheit Temperature:'))
+celsius = convert_to_cel(fahrenheit)
+print(f'{fahrenheit} degrees Fahrenheit is {celsius:.2f} degrees Celsius. ')
+
+celsius = float(input('Enter a Celsius Temperature:'))
+fahrenheit = convert_to_F(celsius)
+print(f'{celsius} degrees Celsius {fahrenheit:.2f} is degrees Fahrenheit. ')
